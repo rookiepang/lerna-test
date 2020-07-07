7c89a40fa8a01e826c50ffb4a790435d07776eab
pangjiayao3 <pangjiayao3@jd.com>
2020-07-07 16:53:48 +0800

    develop

--- packages/example-lib/src/MyHelloView/MyHelloView.js
+++ packages/example-lib/src/MyHelloView/MyHelloView.js
@@ -8,7 +8,7 @@ const cx = classNames.bind(css);
 export const MyHelloView = props => (
   <div className={cx('hello-local', 'hello-global')}>
     <img className={cx('hello-img')} src={reactImg} alt="React" />
-    <h1>HelloViewLib</h1>
+    <h1>HelloViewLib!</h1>
     <div>{props.children}</div>
   </div>
 );
