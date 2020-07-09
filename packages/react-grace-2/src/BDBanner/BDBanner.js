63df84acf41736f9b2bac0ff8e38505e69ef8880
pangjiayao3 <pangjiayao3@jd.com>
2020-07-09 11:42:32 +0800

    modify

--- packages/react-grace-2/src/BDBanner/BDBanner.js
+++ packages/react-grace-2/src/BDBanner/BDBanner.js
@@ -3,7 +3,7 @@ import './BDBanner.css';
 
 export const BDBanner = props => (
   <div>
-    <h1>公共组件</h1>
+    <h2>公共组件</h2>
     <div className={props.background}>{props.children}</div>
   </div>
 );
