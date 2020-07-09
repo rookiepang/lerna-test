c1eecb46fddb8f3e099e9c641c96db6e2241744c
pangjiayao3 <pangjiayao3@jd.com>
2020-07-09 11:54:36 +0800

    add packages

--- doc-server/index.js
+++ doc-server/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BDBanner } from 'react-grace-2';
+
 const containerDom = document.createElement('div');
 
 ReactDOM.render(
-    <h1>lerna组件框架</h1>,
+    <BDBanner background='blue'>
+        这里是文档
+    </BDBanner>,
     document.body.appendChild(containerDom)
 )
